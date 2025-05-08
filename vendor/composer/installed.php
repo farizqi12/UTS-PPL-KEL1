73cc383af56d55381a5c07ee607d6011429e2681
farizqi12 <muhammadfarizqi39@gmail.com>
2025-05-08 23:19:46 +0700

    laravel init2

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v12.0.7',
-        'version' => '12.0.7.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '69714d73632c7022716cf6299cb8859a5fc2bc11',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -386,9 +386,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v12.0.7',
-            'version' => '12.0.7.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '69714d73632c7022716cf6299cb8859a5fc2bc11',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
